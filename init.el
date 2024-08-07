0851a73ff69c79090c846a639eba35e2a42e7c69
FelDuck <felixthdev@gmail.com>
2024-08-07 14:27:04 -0400

    my dot files for emacs as they stand when I created this repositary

---
+++ init.el
@@ -0,0 +1,20 @@
+(require 'org)
+(require 'ob-tangle)
+(org-babel-load-file "~/.config/emacs/config.org")
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" "551629d1e63bb66423dd80b3ec2d1a67611d1fa570e7238201e65b25a3b3834f" "ae3cd5fdf7b70b83914f052d4151db9c815f7b253233977fbb238bc4bd64bb09" "1a6d120936f9df3f44953124dbf9e56b399e021702ca7d1844e6c5e1658b692b" default))
+ '(package-selected-packages '(magit org-bullets tao-theme company dracula-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
