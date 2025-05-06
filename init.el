5a37dfed27c19110f8c5a5b3fdbd9d6e9a1a3550
FelDuck <felixthdev@gmail.com>
2025-05-06 16:00:50 -0400

    small modifs added md exporter package

--- init.el
+++ init.el
@@ -12,7 +12,8 @@
    '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" "551629d1e63bb66423dd80b3ec2d1a67611d1fa570e7238201e65b25a3b3834f" "ae3cd5fdf7b70b83914f052d4151db9c815f7b253233977fbb238bc4bd64bb09" "1a6d120936f9df3f44953124dbf9e56b399e021702ca7d1844e6c5e1658b692b" default))
  '(org-agenda-files
    '("/home/lexduck/org/SDD1005.org" "/home/lexduck/org/CV.org" "/home/lexduck/org/IINF1006.org" "/home/lexduck/org/INF1010.org" "/home/lexduck/org/PIF1006.org" "/home/lexduck/org/SDD1003.org" "/home/lexduck/org/acornsquash.org" "/home/lexduck/org/active.org" "/home/lexduck/org/industry.org" "/home/lexduck/org/interview.org" "/home/lexduck/org/jounal.org" "/home/lexduck/org/npcDump.org" "/home/lexduck/org/worknotes.org"))
- '(package-selected-packages '(exwm magit org-bullets tao-theme company dracula-theme))
+ '(package-selected-packages
+   '(sqlite3 plantuml-mode exwm magit org-bullets tao-theme company dracula-theme))
  '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
